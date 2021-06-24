fbb2248f0ed4f9643e65bedfe8ec702ac07e5c43
Akane Murakawa <chenshjing@gmail.com>
2021-06-24 18:29:55 +0800

    Update and rename run_virtual_vm_on_window_started.bat to run_virtual_vm_on_windows_started.bat

--- run_virtual_vm_on_windows_started.bat
+++ run_virtual_vm_on_windows_started.bat
@@ -1,4 +1,4 @@
-# 将此文件`run_virtual_vm_on_window_started.bat`放到C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
+# 将此文件`run_virtual_vm_on_windows_started.bat`放到C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 cd C:\Program Files\Oracle\VirtualBox
 
 echo "starting ubuntu ......"
